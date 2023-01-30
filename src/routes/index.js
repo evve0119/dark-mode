8a55d964c13005923f6bc360a483207cbc2d559b
evve0119 <58882040+evve0119@users.noreply.github.com>
2023-01-30 11:44:17 +0800

    Initial commit on Jan 30

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+import Home from './App';
+import { Route } from "react-router-dom";
+
+// Use something like react-router-dom to manage multiple pages/routes
+    <Route path='/' element={<Home />}/>
+
+export default Home;
